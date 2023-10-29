be9ed8b3376a0e8108a1b7221ad4bddcddf50685
nftushar <nftushar5200@gmail.com>
2023-10-29 17:16:53 +0600

    Faker Fixed

--- src/FakerChart.js
+++ src/FakerChart.js
@@ -3,23 +3,32 @@ import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Toolti
 import { Bar } from 'react-chartjs-2';
 import faker from 'faker';
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
 export const options = {
     responsive: true,
-    plugins: {
-        legend: {
-            position: 'top',
+    indexAxis: 'x',
+    elements: {
+        bar: {
+            borderWidth: 2
         },
+    },
+    plugins: {
         title: {
             display: true,
             text: 'Chart.js Bar Chart',
+        },
+        responsive: true,
+        scales: {
+            x: {
+                stacked: true,
+            },
+            y: {
+                stacked: true,
+            },
+        },
+        legend: {
+            position: 'top',
         },
     },
 };
@@ -32,17 +41,19 @@ const data = {
         {
             label: 'Dataset 1',
             data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+            borderColor: 'rgb(255, 99, 132)',
             backgroundColor: 'rgba(255, 99, 132, 0.5)',
         },
         {
             label: 'Dataset 2',
             data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+            borderColor: 'rgb(53, 162, 235)',
             backgroundColor: 'rgba(53, 162, 235, 0.5)',
         },
     ],
 };
 
-  function App() {
+function App() {
     return <Bar options={options} data={data} />;
 }
 
